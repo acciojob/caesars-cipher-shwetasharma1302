a7782756209d4a7104581efaea90d6e9ba8bb41a
shwetasharma1302 <126080003+shwetasharma1302@users.noreply.github.com>
2023-08-17 16:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,14 +33,14 @@ const lookup = {
 
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  
+  for (let char of encodedStr) {
+    // Convert uppercase letters, leave non-alphabetic characters as is
+    decodedArr.push(lookup[char] || char);
+  }
 
-  return; //return decodedArr
+  return decodedArr.join('');
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
+// Example usage
+console.log(rot13("SERR YBIR? NPPVBWBO")); // Output: "FREE LOVE? CHICKEN"
